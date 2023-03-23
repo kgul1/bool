47d889b03e8d393b607e08a69b59f72d4996c0c8
konradstarzec <konrad.starzec@zst.kolbuszowa.pl>
2023-03-23 12:18:33 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+/*napisz progrma prawda falsz dla takich danych jsli jeden to true jesli 0 flase*/
+using  namespace std;
+int main() {
+  bool kacper= true;
+  bool nazwisko = false;
+  cout<< kacper<<" "<< nazwisko ;
+    return 0;
+}
